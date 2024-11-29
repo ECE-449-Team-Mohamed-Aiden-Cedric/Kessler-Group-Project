cac2c5b32a810439a2bb7acbb60ed9cfd46d0743
Cedric Boucher <cedricjboucher@pm.me>
2024-11-29 16:19:09 -0700

    setup genetic algorithm running with pygad

--- src/config.py
+++ src/config.py
@@ -4,3 +4,13 @@ RUN_WITH_GRAPHICS: bool = True
 
 USE_SIMULATION_CACHE: bool = True # default is True
 FLUSH_SIMULATION_CACHE_AFTER_RUN: int = 1000 # default is 1000
+
+GA_POPULATION_SIZE: int = 42
+GA_GENERATION_GOAL: int = 100
+GA_FITNESS_GOAL: float = 1
+GA_NUMBER_OF_PARENTS: int = 21
+GA_NUMBER_OF_GENES_TO_MUTATE: int = 1
+
+GA_CHROMOSOME_LENGTH: int = 1
+
+GA_MODEL_FILE: str = "team_cam_controller.pygad"
--- src/genetic_learner.py
+++ src/genetic_learner.py
@@ -7,8 +7,11 @@ from kesslergame.team import Team
 from team_cam_controller import TeamCAMController
 from kesslergame.graphics import GraphicsType
 from kesslergame.kessler_game import TrainerEnvironment
+import config
 
 from typing import Any
+
+import pygad
 
 Gene = dict[str, tuple[float, float, float]]
 Chromosome = list[Gene] # MUST be a list due to implementation of EasyGA
@@ -48,30 +51,32 @@ def execute_fuzzy_inference(
     return team_score
 
 def fitness_score_function(score: Team) -> float:
-    """function to compute a fitness score to be minimized
+    """function to compute a fitness score to be maximized
 
     Args:
         score (Team): the Team object containing the score parameters from the game
 
     Returns:
-        float: fitness score to be minimized
+        float: fitness score to be maximized
     """
     fitness_score: float = (
-        score.asteroids_hit * score.accuracy /
+        score.asteroids_hit * score.accuracy
         + score.deaths * -30
     )
 
     return fitness_score
 
-def fitness(chromosome: Chromosome) -> float:
+def fitness(ga_instance: pygad.GA, chromosome: Chromosome, solution_idx: int) -> float:
     """runs the controller with the given chromosome
-    and returns a fitness score to be minimized
+    and returns a fitness score to be maximized
 
     Args:
+        ga_instance (pygad.GA): pygad.GA instance
         chromosome (Chromosome): chromosome to use for the controller fuzzy system
+        solution_idx (int): idk lol
 
     Returns:
-        float: fitness score to be minimized
+        float: fitness score to be maximized
     """
     controller: TeamCAMController = TeamCAMController(chromosome)
     scenario: Scenario = Scenario(
@@ -103,4 +108,36 @@ def fitness(chromosome: Chromosome) -> float:
 
     score: Team = execute_fuzzy_inference(game, scenario, controller)
 
-    return fitness_score_function(score)
+    final_fitness_score: float = fitness_score_function(score)
+
+    print(f"iteration fitness: {final_fitness_score}")
+
+    return final_fitness_score
+
+def on_generation(ga_instance: pygad.GA):
+    print("Generation: {:d}".format(ga_instance.generations_completed))
+    print("Fitness of best solution: {:.2f}".format(ga_instance.best_solution()[-1]))
+
+def run_genetic_algorithm():
+    ga_instance: pygad.GA = pygad.GA(
+        num_generations=config.GA_GENERATION_GOAL,
+        num_parents_mating=config.GA_NUMBER_OF_PARENTS,
+        fitness_func=fitness,
+        sol_per_pop=config.GA_POPULATION_SIZE,
+        num_genes=config.GA_CHROMOSOME_LENGTH,
+        on_generation=on_generation,
+        mutation_num_genes=config.GA_NUMBER_OF_GENES_TO_MUTATE
+    )
+
+    ga_instance.run()
+
+    solution, solution_fitness, solution_idx = ga_instance.best_solution(ga_instance.last_generation_fitness)
+    print(f"Parameters of the best solution : {solution}")
+    print(f"Fitness value of the best solution = {solution_fitness}")
+    print(f"Index of the best solution : {solution_idx}")
+    ga_instance.save(config.GA_MODEL_FILE)
+    ga_instance.plot_fitness()
+
+
+if __name__ == "__main__":
+    run_genetic_algorithm()
