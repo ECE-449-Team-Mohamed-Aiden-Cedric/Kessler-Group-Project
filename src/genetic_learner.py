e617a49aa80cf0e75ad99162a4c731fd4004d2dc
Cedric Boucher <cedricjboucher@pm.me>
2024-11-29 15:24:48 -0700

    create execute fuzzy inference function

--- src/genetic_learner.py
+++ src/genetic_learner.py
@@ -1,4 +1,8 @@
 import random
+from kesslergame.score import Score
+from kesslergame.kessler_game import KesslerGame
+from kesslergame.scenario import Scenario
+from kesslergame.controller import KesslerController
 
 Gene = dict[str, tuple[float, float, float]]
 Chromosome = list[Gene] # MUST be a list due to implementation of EasyGA
@@ -19,3 +23,18 @@ def gene_generation() -> Gene:
     }
 
     return gene
+
+def execute_fuzzy_inference(
+    kessler_game: KesslerGame,
+    scenario: Scenario,
+    controller: KesslerController
+    ) -> Score:
+    """executes the fuzzy system and returns the results we care about
+
+    Returns:
+        tuple[int, float]: number of asteroids hit, accuracy
+    """
+    score: Score
+    score, _ = kessler_game.run(scenario=scenario, controllers=[controller])
+
+    return score
