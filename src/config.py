8f8f20a0d5e78b1ca37f5b578c2f66bfad7cd18f
Cedric Boucher <cedricjboucher@pm.me>
2024-11-29 18:50:52 -0700

    reenable accidentally disabled parellel processing

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 LOG_FILE_PATH: str = "./KesslerController.log"
 
-RUN_WITH_GRAPHICS: bool = True
+RUN_WITH_GRAPHICS: bool = False
 
 USE_SIMULATION_CACHE: bool = True # default is True
 FLUSH_SIMULATION_CACHE_AFTER_RUN: int = 1000 # default is 1000
@@ -9,10 +9,10 @@ SEED: int = 42069
 
 # fitness of unoptimized Dr. Dick model on seed 42069: 40.91447368421052
 
-GA_POPULATION_SIZE: int = 3
-GA_GENERATION_GOAL: int = 5
-GA_FITNESS_GOAL: float = 1
-GA_NUMBER_OF_PARENTS: int = 2
+GA_POPULATION_SIZE: int = 20
+GA_GENERATION_GOAL: int = 10
+GA_FITNESS_GOAL: float = 10000
+GA_NUMBER_OF_PARENTS: int = 5
 GA_NUMBER_OF_GENES_TO_MUTATE: int = 1
 
 GA_NUMBER_OF_THREADS: int = 10 # number of threads/processes to use for GA
